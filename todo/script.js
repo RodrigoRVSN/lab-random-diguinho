b32403bf781c973ecd401925f39399db3bd8e3ca
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-06-16 21:08:19 -0300

    feat: add todo-list vanilla

---
+++ todo/script.js
@@ -0,0 +1,29 @@
+const submitForm = (event) => {
+  event.preventDefault()
+  document.getElementsByClassName('todo-not-found')[0].style.display = 'none'
+
+  const input = document.getElementsByClassName('todo-input')[0]
+  const value = input.value
+
+  if(!value.trim()) return
+
+  const container = document.getElementsByClassName('todo-container')[0]
+
+  const element = document.createElement('p')
+  element.className = 'todo-item'
+  element.innerHTML = `<span>${value}</span> <button class="todo-remove" onclick="removeElement()">❌</button>`
+
+  container.appendChild(element)
+
+  input.value = ''
+}
+
+const removeElement = () => {
+  event.currentTarget.parentElement.remove()
+
+  const numberOfItens = document.getElementsByClassName('todo-container')[0].childElementCount
+  
+  if(numberOfItens === 0) {
+    document.getElementsByClassName('todo-not-found')[0].style.display = 'block'
+  }
+}
